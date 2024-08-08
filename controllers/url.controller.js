a8f2685214e35ed049b0c471fec5e4aa9ec88224
Kirti <bhosalekirti2002@gmail.com>
2024-08-08 13:42:46 +0530

    helper function added

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,20 +1,50 @@
-const URL = require("../models/url.model");
 const shortid = require("shortid");
+const URL = require("../models/url.model");
 
 const BASE_URL = "http://localhost:3000/api/url";
 
+// Function to generate a unique short URL
+const generateUniqueShortUrl = async () => {
+  let unique = false;
+  let shortUrl;
+  while (!unique) {
+    const shortId = shortid.generate();
+    shortUrl = `${BASE_URL}/${shortId}`;
+    const existingUrl = await URL.findOne({ shortUrl });
+    if (!existingUrl) {
+      unique = true;
+    }
+  }
+  return shortUrl;
+};
+
 exports.shortenUrl = async (req, res) => {
   const { originalUrl } = req.body;
-  const shortId = shortid.generate();
-  const shortUrl = `${BASE_URL}/${shortId}`;
 
   try {
-    const url = new URL({
+    // Check if the original URL already exists in the database
+    let url = await URL.findOne({ originalUrl, userId: req.user.id });
+
+    if (url) {
+      // If the URL already exists, return the existing short URL
+      return res.status(200).json({
+        success: true,
+        message: "URL already shortened",
+        originalUrl,
+        shortUrl: url.shortUrl,
+      });
+    }
+
+    // If the URL does not exist, create a new unique short URL
+    const shortUrl = await generateUniqueShortUrl();
+
+    url = new URL({
       originalUrl,
       shortUrl,
       userId: req.user.id,
     });
     await url.save();
+
     res.status(200).json({
       success: true,
       message: "URL shortened successfully",
